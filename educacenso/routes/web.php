eff7fef160abdf67d239c884a7990ec7372e8ca5
Joao Silva <joao.210105@bento.local>
2022-07-26 09:40:38 -0300

    removi turmaController

--- educacenso/routes/web.php
+++ educacenso/routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\TurmaController;
 use App\Http\Controllers\PeriodoController;
 use App\Http\Controllers\CursoController;
-use App\Http\Controllers\TurmaController;
 use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
