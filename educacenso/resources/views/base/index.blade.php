e2a655f8c9f1decce2a3bb4c26759358ef8c7f3a
juliaconzatti <juliarconzatti@gmail.com>
2022-07-26 10:04:02 -0300

    hdfob

--- educacenso/resources/views/base/index.blade.php
+++ educacenso/resources/views/base/index.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-@include('base.header', ['title' => 'EducaCnso'])
+@include('base.header', ['title' => 'EducaCenso'])
 <body>
     <div class="container">
         @yield('container')
     </div>
 
     @include('base.footer')
-    
+
 </body>
 </html>
