cf93b646bedfde90832c2ddfa0edf81f0da55f48
juliaconzatti <juliarconzatti@gmail.com>
2022-07-27 09:14:20 -0300

    pra alyssa

---
+++ educacenso/resources/views/respostas/edit.blade.php
@@ -0,0 +1,17 @@
+@extends ('base.index')
+
+ @section('container')
+
+ <form action ='/respostas/update' method='post' class="form my-5">
+    <input type='hidden' name='_token' value='{{ csrf_token() }}'/>
+   @include('components.field', ['type' => 'text', 'id' => 'nome_aluno', 'name' => 'nome_aluno', 'label' => 'Nome', 'value' => $resposta->nome_aluno])
+   @include('components.field', ['type' => 'text', 'id' => 'cpf', 'name' => 'cpf', 'label' => 'CPF', 'value' => $resposta ->cpf])
+   <!-- PEDIR ESTADO COM API -->
+   <!-- BUSCAR AS CIDADES -->
+   @include('components.field', ['type' => 'radio', 'id' => 'prefeitura', 'name' => 'poder_publico_responsavel', 'label' => 'Prefeitura', 'value' => "Prefeitura"]) <!-- ver dos radios-->
+   @include('components.field', ['type' => 'radio', 'id' => 'estado', 'name' => 'poder_publico_responsavel', 'label' => 'Estado', 'value' => "Estado"])
+
+   <a class="btn btn-danger" href="/resposta">Voltar</a>
+   @include('components.button', ['type' => 'submit', 'color' => 'success', 'texto' => 'Alterar'])
+ </form>
+ @endsection
