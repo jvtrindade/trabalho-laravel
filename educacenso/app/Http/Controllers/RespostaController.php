38b557628efea482c50db9760596e98109246f12
jvtrindade <jv.trindadesilva@gmail.com>
2022-07-30 21:37:45 -0300

    controllerResposta

--- educacenso/app/Http/Controllers/RespostaController.php
+++ educacenso/app/Http/Controllers/RespostaController.php
@@ -28,4 +28,50 @@ class RespostaController extends Controller
     function create(){
         return view('respostas.create');
     }
+
+    function store(Request $request){
+        $data = $request->all();
+        unset($data['_token']);
+
+        DB::table('respostas')->insert($data);
+
+        return redirect('/respostas');
+    }
+
+    function edit($id){
+        $resposta = DB::table('resposta')
+        ->find($id);
+
+        return view('respostas.edit', [
+            'resposta' => $resposta
+        ]);
+    }
+
+    function update(Request $request){
+        $data = $request->all();
+        unset($data['_token']);
+
+        $id = array_shift($data);
+
+        DB::table('respostas')
+        ->where('id', $id)
+        ->update($data);
+
+        redirect('/respostas');
+    }
+
+    function destroy($id){
+        DB::table('respostas')
+        ->where('id', $id)
+        ->delete();
+
+        return redirect('/respostas');
+    }
+
+    function show($id){
+        $resposta = DB::table('respostas')
+        ->find($id);
+
+        return view('respostas.show', ['resposta' => $resposta]);
+    }
 }
