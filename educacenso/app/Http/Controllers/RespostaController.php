8a68eda80da60c72a7401c697bdce09d6f404be8
Alyssa Michalski <alyssa.070505@bento.local>
2022-08-02 09:02:42 -0300

    controle data

--- educacenso/app/Http/Controllers/RespostaController.php
+++ educacenso/app/Http/Controllers/RespostaController.php
@@ -10,6 +10,10 @@ use Illuminate\Support\Facades\DB;
 class RespostaController extends Controller
 {
     function index(){
+
+    }
+
+    function create(){
         $hoje = Carbon::now()->format('Y-m-d');
         $periodos = DB::table("periodos")->select("dt_inicio", "dt_fim")->get();
         $ativo = false;
@@ -21,28 +25,25 @@ class RespostaController extends Controller
         }
         if($ativo == false){
             echo('Não há nenhum período ativo no momento.');
+        }else{
+            $periodos = DB::table('periodos')
+            ->select()
+            ->get();
+
+            //$urlEstados = "https://servicodados.ibge.gov.br/api/v1/localidades/estados";
+            //$estados = Request($urlEstados);
+            //print $estados; // não consegui achar uma forma e fazer esse request
+
+            // $urlCidades = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/".$uf."/municipios";
+            // $cidades = Request($urlCidades);
+            // $cidades = json_encode($cidades);
+
+            return view('respostas.create', [
+                //'estados' => $estados,
+                // 'cidades' => $cidades
+                'periodos' => $periodos
+            ]);
         }
-    }
-
-    function create(){
-
-        $periodos = DB::table('periodos')
-        ->select()
-        ->get();
-
-        //$urlEstados = "https://servicodados.ibge.gov.br/api/v1/localidades/estados";
-        //$estados = Request($urlEstados);
-        //print $estados; // não consegui achar uma forma e fazer esse request
-
-        // $urlCidades = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/".$uf."/municipios";
-        // $cidades = Request($urlCidades);
-        // $cidades = json_encode($cidades);
-
-        return view('respostas.create', [
-            //'estados' => $estados,
-            // 'cidades' => $cidades
-            'periodos' => $periodos
-        ]);
     }
 
     function store(Request $request){
