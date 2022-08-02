1d28d0156e2a5c6f6977765491704773361ef3cb
Alyssa Michalski <alyssa.070505@bento.local>
2022-08-02 09:59:13 -0300

    tabela errada

--- educacenso/app/Http/Controllers/RespostaController.php
+++ educacenso/app/Http/Controllers/RespostaController.php
@@ -75,7 +75,7 @@ class RespostaController extends Controller
     }
 
     function edit($id){
-        $resposta = DB::table('resposta')
+        $resposta = DB::table('respostas')
         ->find($id);
 
         return view('respostas.edit', [
