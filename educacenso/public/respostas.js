da7ae3a70fdef68b4ca7709dc3360f8184b5b4cb
Joao Pereira <joao.090405@bento.local>
2022-08-03 10:01:22 -0300

    js

--- educacenso/public/respostas.js
+++ educacenso/public/respostas.js
@@ -1,3 +1,18 @@
 onload = async () => {
-    btn
+    curso = document.getElementById("curso_id");
+
+    curso.addEventListener("click", async () => {
+        const inputCurso = document.getElementById("curso_id").value;
+
+        const body = new FormData();
+        body.append("turma_cadastro", inputCurso);
+
+        const response = await fetch(`options.php`, {
+            method: "POST",
+            body,
+          });
+
+        turma = await response.json()
+        
+    });
 };
